057bc72ca29aa4209797824942159b05c0019b6f
dien0512 <dien05122005@gmail.com>
2024-07-27 10:56:26 +0700

    BTVN buoi3

--- BTVN buoi3/bai4.py
+++ BTVN buoi3/bai4.py
@@ -1,4 +1,6 @@
-a = str(input('Nhập một chuỗi văn bản chứa họ và tên của một thành viên trong câu lạc bộ: '))
+a = input('Nhập một chuỗi văn bản chứa họ và tên của một thành viên trong câu lạc bộ: ')
 a1 = a.split()
-for i in range(len(a)):
-    print(a[i], end=" ")
+for i in range(len(a1)):
+    a1[i] = a1[i].title()
+for i in a1:
+    print(i,end=' ')
